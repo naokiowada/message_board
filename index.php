fd4eaa7db37d305caf928902e39e03bb51038d83
naokiowada <naoki.owada.709@gmail.com>
2022-03-03 13:20:46 +0900

    [Update] メッセージ投稿の処理を実装

--- index.php
+++ index.php
@@ -1,8 +1,31 @@
 <?php
 session_start();
 require('library.php');
+if (isset($_SESSION['id']) && isset($_SESSION['name'])) {
+    $id = $_SESSION['id'];
+    $name = $_SESSION['name'];
+} else {
+    header('Location: login.php');
+    exit();
+}
 
-$name = $_SESSION['name'];
+// メッセージの投稿
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $message = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_STRING);
+    $db = dbconnect();
+    $stmt = $db->prepare('insert into posts (message, member_id) values(?,?)');
+    if (!$stmt) {
+        die($db->error);
+    }
+    $stmt->bind_param('si', $message, $id);
+    $success = $stmt->execute();
+    if (!$success) {
+        die($db->error);
+    }
+    // 現在のページにリダイレクトすることにより、更新をかけてもデータベースに新たなレコードを表示させない仕組み
+    header('Location: index.php');
+    exit();
+}
 ?>
 <!DOCTYPE html>
 <html lang="ja">
